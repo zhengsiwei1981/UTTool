f34bb020bcc4e6ffa95e4a57a60e1261069707cc
zhengsiwei19810905 <philip.zheng@greendotcorp.com>
2023-06-30 16:54:34 +0800

    Update Program.cs

--- UTTool/UITool.Test/Program.cs
+++ UTTool/UITool.Test/Program.cs
@@ -9,8 +9,7 @@ namespace UITool.Test
     {
         static void Main(string[] args)
         {
-            new UTTool.Core.UTToolLoader().Load(@"C:\P4Source\GSS\Trunk\MMSSVC\Web\Services\ACHTransfer\V1\Business\ACHTransfer.Controllers\bin\Debug\net6.0\ACHTransfer.Controllers.dll");
-            //new UTTool.Core.UTToolLoader().LoadDirectory(@"C:\P4Source\GSS\Trunk\MMSSVC\Web\Services\ACHTransfer\V1\Business\ACHTransfer.Controllers\bin\Debug\net6.0");
+            new UTTool.Core.UTToolLoader().Load("");
         }
 
 
